324dd105a1fd082978af89018c89c490573375ed
Kawish-Raj <rajkawish@gmail.com>
2024-06-16 14:58:26 +1000

    calculations work need to do clear and equal

--- script.js
+++ script.js
@@ -11,37 +11,64 @@ function multiply(x,y) {
 }
 
 function divide(x,y) {
-    return x/y;
+    var result = x/y;
+    return result.toFixed(2);
 }
 
-var var1, operator, var2;
+var var1 = "";
+var operator = "";
+var var2 = "";
 
 function operate(var1, operator, var2) {
     var returnVal;
-    if (operator === "add"){
+    if (operator === "+"){
         returnVal = add(var1, var2);
     }
-    else if (operator === "subtract"){
+    else if (operator === "-"){
         returnVal = subtract(var1, var2);
     }
-    else if (operator === "multiply"){
+    else if (operator === "*"){
         returnVal = multiply(var1, var2);
     }
     else {
         returnVal = divide(var1, var2);
     }
+
+    return returnVal;
 }
 
-function updateDisplay(){
-
-}
+var operator_exist = false;
 
 const display = document.querySelector("div.display");
 const num_buttons_list = document.querySelectorAll("button.number");
+const operator_buttons_list = document.querySelectorAll("button.operator");
 
 num_buttons_list.forEach(num => {
     num.addEventListener("click", function(){
-        display.textContent = this.textContent;
+        display.textContent = display.textContent + this.textContent;
+        if (!operator_exist){
+            var1 = var1 + this.textContent;
+        }
+        else {
+            var2 = var2 + this.textContent;
+        }
+    })
+})
+
+operator_buttons_list.forEach(operator_node => {
+    operator_node.addEventListener("click", function() {
+        if(!operator_exist){
+            display.textContent = display.textContent + this.textContent;
+            operator_exist = true;
+            operator = this.textContent;
+        }
+        else if (var2 !== ""){
+            var result = operate(parseInt(var1),operator,parseInt(var2));
+            display.textContent = result.toString() + this.textContent;
+            var1 = result.toString();
+            var2 = "";
+            operator = this.textContent;
+        }
     })
 })
 
